f7de071b2d58369daee6752d03b1b3e54f084de4
aakkiiff <jackakif@gmail.com>
2023-11-03 15:17:12 +0600

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hello, Welcome to the demo v1
+         Hello, Welcome to the demo v1.0.1
         </p>
         <a
           className="App-link"
