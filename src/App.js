76d35dd4bf41ec141b58331d2ec4a9ec255748b5
aakkiiff <jackakif@gmail.com>
2024-05-30 23:35:19 +0600

    changes made

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hello, from cloudops project !
-         iam akif
+         Hello, from cloudops project!
         </p>
         <a
           className="App-link"
