e79a25814d72176672c96e91e68db10bd91da92f
akif <jackakif@gmail.com>
2024-09-27 21:54:31 +0600

    version 2 released

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hello, from devops project.
+         Hello, from devops project. version 2
         </p>
         <a
           className="App-link"
