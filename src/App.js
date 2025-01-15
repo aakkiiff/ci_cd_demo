71425565f1fa1868be422a464be3b201cddfc05a
akif <104318101+aakkiiff@users.noreply.github.com>
2025-01-15 11:26:15 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hello, from devops project. version 5!!!!!
+         Hello, from devops project. version quickopsv6!!!!!
         </p>
         <a
           className="App-link"
