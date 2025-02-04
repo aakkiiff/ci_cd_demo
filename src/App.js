b7334d382ef5b70b532e0b5e7ad6225af4fef088
akif <104318101+aakkiiff@users.noreply.github.com>
2025-02-04 13:59:49 +0600

    test 1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hello, from devops project. version newwww!!!!!
+         Hello, from devops project.
         </p>
         <a
           className="App-link"
